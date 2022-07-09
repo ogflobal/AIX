6988bebbff563140b06a4266f105197d27f9c737
ogflobal <74718043+ogflobal@users.noreply.github.com>
2022-07-09 02:24:31 -0500

    Create 10_HM.sh

---
+++ 10_HM.sh
@@ -0,0 +1,10 @@
+qemu-system-ppc64 -cpu POWER8 \
+-machine pseries -m 4096 -serial stdio \
+-drive file=hdisk0.qcow2,if=none,id=drive-virtio-disk0 \
+-device virtio-scsi-pci,id=scsi \
+-device scsi-hd,drive=drive-virtio-disk0 \
+-cdrom aix_7200-04-02-2027_1of2_072020.iso \
+-net nic -net tap,script=no,ifname=tap0
+-prom-env "boot-command=boot disk:" \
+-prom-env "input-device=/vdevice/vty@71000000" \
+-prom-env "output-device=/vdevice/vty@71000000"
